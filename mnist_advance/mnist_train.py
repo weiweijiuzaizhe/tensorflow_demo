999c45b5f5714e98a13fddcd604f9e1e46976578
wuwei <weiweijiuzaizhe@gmail.com>
2017-08-18 17:05:42 +0800

    Update mnist_train.py
    change code

--- mnist_advance/mnist_train.py
+++ mnist_advance/mnist_train.py
@@ -4,34 +4,41 @@ import os
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-# ??mnist_inference.py???????????????
 import mnist_inference
 
-# ??????????
+# 配置神经网络的参数。
 BATCH_SIZE = 100
 LEARNING_RATE_BASE = 0.8
 LEARNING_RATE_DECAY = 0.99
 REGULARAZTION_RATE = 0.0001
 TRAINING_STEPS = 30000
 MOVING_AVERAGE_DECAY = 0.99
-# ????????????
-MODEL_SAVE_PATH = "/tmp/tensorflow/mnist/"
+TRAIN = 1
+
+
+# 模型保存的路径和文件名。
+MODEL_SAVE_PATH = "/path/to/model/"
 MODEL_NAME = "model.ckpt"
 
 
 def train(mnist):
-    # ??????placeholder?
-    x = tf.placeholder(
-        tf.float32, [None, mnist_inference.INPUT_NODE], name='x-input')
+    # 定义输入输出placeholder。
+    x = tf.placeholder(tf.float32, [
+        BATCH_SIZE,  # 第一维表示一个batch中样例的个数。
+        mnist_inference.IMAGE_SIZE,  # 第二维和第三维表示图片的尺寸。
+        mnist_inference.IMAGE_SIZE,
+        mnist_inference.NUM_CHANNELS],  # 第四维表示图片的深度，对于RBG格
+                       # 式的图片，深度为5。
+                       name='x-input')
     y_ = tf.placeholder(
         tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
 
     regularizer = tf.contrib.layers.l2_regularizer(REGULARAZTION_RATE)
-    # ????mnist_inference.py???????????
-    y = mnist_inference.inference(x, regularizer)
+    # 直接使用mnist_inference.py中定义的前向传播过程。
+    y = mnist_inference.inference(x, TRAIN, regularizer)
     global_step = tf.Variable(0, trainable=False)
 
-    # ????????????????????????
+    # 定义损失函数、学习率、滑动平均操作以及训练过程。
     variable_averages = tf.train.ExponentialMovingAverage(
         MOVING_AVERAGE_DECAY, global_step)
     variables_averages_op = variable_averages.apply(
@@ -49,27 +56,35 @@ def train(mnist):
     with tf.control_dependencies([train_step, variables_averages_op]):
         train_op = tf.no_op(name='train')
 
-        # ???TensorFlow?????
+        # 初始化TensorFlow持久化类。
     saver = tf.train.Saver()
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
 
-        # ?????????????????????,??????????????
-        # ????????
+        # 在训练过程中不再测试模型在验证数据上的表现，验证和测试的过程将会有一个独
+        # 立的程序来完成。
         for i in range(TRAINING_STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
+
+            reshaped_xs = np.reshape(xs, (BATCH_SIZE,
+                                          mnist_inference.IMAGE_SIZE,
+                                          mnist_inference.IMAGE_SIZE,
+                                          mnist_inference.NUM_CHANNELS))
+
+
+
             _, loss_value, step = sess.run([train_op, loss, global_step],
-                                           feed_dict={x: xs, y_: ys})
-            # ?1000????????
+                                           feed_dict={x: reshaped_xs, y_: ys})
+            # 每1000轮保存一次模型。
             if i % 1000 == 0:
-                # ???????????????????????batch?????
-                # ?????????????????????????????????
-                # ??????????????????
+                # 输出当前的训练情况。这里只输出了模型在当前训练batch上的损失函
+                # 数大小。通过损失函数的大小可以大概了解训练的情况。在验证数据集上的
+                # 正确率信息会有一个单独的程序来生成。
                 print("After %d training step(s), loss on training "
                       "batch is %g." % (step, loss_value))
-                # ???????????????global_step??,????????
-                # ?????????????????,??model.ckpt-1000??
-                # ??1000?????????
+                # 保存当前的模型。注意这里给出了global_step参数，这样可以让每个被
+                # 保存模型的文件名末尾加上训练的轮数，比如“model.ckpt-1000”表示
+                # 训练1000轮之后得到的模型。
                 saver.save(
                     sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME),
                     global_step=global_step)
